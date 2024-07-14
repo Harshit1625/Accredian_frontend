640a4a170a9e54b7b5da5c2675ded7d1f3f5f4a7
Harshit Srivastava <harshitsri1625@gmail.com>
2024-07-14 19:16:01 +0530

    third

--- src/components/HowDoISection.jsx
+++ src/components/HowDoISection.jsx
@@ -15,19 +15,19 @@ const HowDoISection = () => {
 
       {/* absolute images */}
       <div className="absolute flex flex-col items-center lg:top-[220px] xl:top-[210px] top-[102px] sm:top-[140px] md:top-[170px] left-[14vw]  gap-3 lg:gap-4 w-[12vw] lg:w-[11vw] lg:left-[14vw] xl:left-[19vw] text-center ">
-        <img className="lg:h-[10vh] w-[6vw] mt-1 lg:w-[5vw]" src="/src/svgs/AddFriend.svg" />
+        <img className="lg:h-[10vh] w-[6vw] mt-1 lg:w-[5vw]" src="/AddFriend.svg" />
         <p className="text-[1vw] lg:text-[0.8vw] font-semibold">
           Submit referrals through our website referral section easily.
         </p>
       </div>
 
       <div className="absolute flex flex-col items-center lg:top-[230px] xl:top-[214px] top-[102px] sm:top-[140px] md:top-[170px] left-[39vw]  gap-3 lg:gap-4 w-[12vw] lg:w-[11vw] lg:left-[39.3vw] xl:left-[38.6vw] text-center ">
-        <img className="lg:h-[10vh] w-[6vw] mt-1 lg:w-[5vw]" src="/src/svgs/Note.svg" />
+        <img className="lg:h-[10vh] w-[6vw] mt-1 lg:w-[5vw]" src="/Note.svg" />
         <p className="text-[1.1vw] lg:text-[0.8vw] font-semibold">Referral joins an Accredian program.</p>
       </div>
 
       <div className="absolute flex flex-col items-center lg:top-[230px] xl:top-[210px] top-[102px] sm:top-[140px] md:top-[170px] left-[64vw]  gap-2 lg:gap-4 w-[12vw] lg:w-[11vw] lg:left-[64vw] xl:left-[58.5vw] text-center ">
-        <img className="lg:h-[10vh] w-[6vw] mt-1 lg:w-[5vw]" src="/src/svgs/Bonus.svg" />
+        <img className="lg:h-[10vh] w-[6vw] mt-1 lg:w-[5vw]" src="/Bonus.svg" />
         <p className="text-[1.1vw] lg:text-[0.8vw] font-semibold">
           Both parties receive a bonus after 30 days of enrollment.
         </p>
